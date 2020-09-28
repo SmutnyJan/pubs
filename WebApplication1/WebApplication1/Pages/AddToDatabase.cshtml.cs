b4e66a823e935b8071105d7f60f124f1e1069a15
SmutnyJan <jan.smutny@pslib.cz>
2020-09-28 21:42:29 +0200

    first commit

---
+++ WebApplication1/WebApplication1/Pages/AddToDatabase.cshtml.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Models;
+
+namespace WebApplication1.Pages
+{
+    public class AddToDatabaseModel : PageModel
+    {
+        [BindProperty]
+        public string pubName { get; set; }
+        [BindProperty]
+        public string pubLocation { get; set; }
+
+        private readonly AppDbContext _db;
+        public List<Pub> Pubs { get; set; }
+        public AddToDatabaseModel(AppDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+            Pubs = _db.Pubs.ToList();
+        }
+
+        public void OnPost()
+        {
+            _db.Pubs.Add(new Pub {Name = pubName, Municipality = pubLocation});
+            _db.SaveChanges();
+        }
+    }
+}
